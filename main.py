cbb70fd2f1f60758fbc66ef34e0394453aa2b7db
Agnieszka Jurkiewicz <agnieszka.jurkiewicz.stud@pw.edu.pl>
2021-04-19 23:35:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from sklearn.cluster import DBSCAN
+import numpy as np
+
+X = np.array([[1, 2], [2, 2], [2, 3], [8, 7], [8, 8], [25, 80]])
+clustering = DBSCAN(eps=3, min_samples=2).fit(X)
+print(clustering.labels_)
+
+print(clustering)
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
