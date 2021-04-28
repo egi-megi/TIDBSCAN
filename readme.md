d20b1e3d637aba2f1d30d38ac0407e6790000971
greathub <greathub@tuta.io>
2021-04-28 16:38:16 +0200

    Initialize tweets methods

---
+++ readme.md
@@ -0,0 +1,6 @@
+## How to add tensorflow env
+Simply follow this tutorial: [How to Install PyCharm with TensorFlow 2.0 in 2020](https://www.youtube.com/watch?v=zRY5lx-So-c)
+
+## Adding tensorflow model
+To add tensorflow model download it from [TensorFlow Hub website](https://tfhub.dev/google/nnlm-en-dim128/2) and
+place path to unzipped files in ```tweets.py``` file in ```TENSORFLOW_MODEL``` parameter.
---
+++ tweets.py
@@ -0,0 +1,99 @@
+import tensorflow_hub as hub
+import pandas as pd
+import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+# Follow readme and update location of TENSORFLOW MODEL:
+TENSORFLOW_MODEL = 'model/'
+
+
+def read_tweets(filepath):
+    data = pd.read_csv(filepath)
+    tweets_list = data['tweet'].to_list()
+
+    vector = []
+    for tweet in tweets_list:
+        if pd.isnull(tweet):
+            continue
+        vector.append(tweet)
+    return vector
+
+
+def get_vectors(tweets_list):
+    embed = hub.load(TENSORFLOW_MODEL)
+    return embed(tweets_list)
+
+
+def create_and_save_distances_list(vector, save=False):
+    distance_list = []
+    for i in range(len(vector)):
+        p = vector[i]
+        dis = []
+        for q in vector:
+            distance = np.linalg.norm(p - q)
+            dis.append(distance)
+        distance_list.append(dis)
+
+    if save:
+        with open('data/distances_list', 'wb') as file:
+            pickle.dump(distance_list, file)
+
+    return distance_list
+
+
+def read_distances_list():
+    with open('data/distances_list', 'rb') as f:
+        my_list = pickle.load(f)
+
+    return my_list
+
+
+def get_basic_statistics(distances_list):
+    mins = []
+    maxs = []
+    meds = []
+
+    for distance in distances_list:
+        # distance[distance.index(0.0)] = float("nan")
+        distance.pop(distance.index(0.0))
+        mins.append(min(distance))
+        maxs.append(max(distance))
+        meds.append(np.median(distance))
+
+    return [mins, meds, maxs]
+
+
+def plot_basic_statistics(mini, medi, maxi):
+    df = pd.DataFrame(mini, columns=['min'])
+    df['max'] = maxi
+    df['median'] = medi
+    ax = df.plot.hist(bins=60, alpha=0.5)
+    ax.plot()
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Uncomment steps you want to follow:
+
+    # Step 1)
+    # Get vectors from text:
+    # tweets = read_tweets('data/borisjohnson.csv')
+    # tweet_vector = get_vectors(tweets)
+
+    # Step 2)
+    # Find out epsilon by looking at distribution of max, min, median euclidean distances between all points.
+    # distances = create_and_save_distances_list(vector, save=True) # Compute distances
+    distances = read_distances_list() # Read previously computed distances
+    minimum, median, maximum = get_basic_statistics(distances)
+    plot_basic_statistics(minimum, median, maximum)
+
+    # Step 3)
+    # Now after analyzing distribution of distances between points
+    # do clustering n times for epsilon_start and delta_epsilon parameters
+    n = 1
+    epsilon_start = 0.4
+    delta_epsilon = 0.05
+    # clustering = sklearn.cluster.dbscan(X=tweet_vector, eps=1.1, min_samples=2)
+    # print(clustering)
