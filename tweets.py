54bd2637da1988a6912c950ef54eaa8555cb92f6
Jan Nawrocki <greathub@tuta.io>
2021-05-04 21:56:40 +0200

    Add purity score for dbscan

--- tweets.py
+++ tweets.py
@@ -1,3 +1,4 @@
+from sklearn import metrics
 import tensorflow_hub as hub
 import pandas as pd
 import sklearn
@@ -105,12 +106,82 @@ def save_data(data, filename):
 
 
 def get_results_for_multiple_eps(vector, eps_start, eps_end, n, min_pts=2):
-    delta = (eps_end-eps_start) / n
+    delta = (eps_end - eps_start) / n
     results = []
     for i in range(n):
-        r = tidb.algorythm_tidbscan(min_pts, eps_start + n * delta, vector)
+        r = tidb.algorythm_tidbscan(min_pts, eps_start + i * delta, vector)
         results.append(r)
+    return results
+
+
+def get_results_for_multiple_eps_not_our_implementation(vector, eps_start, eps_end, n, min_pts=2):
+    epsilons = get_epsilon_array(eps_start, eps_end, n)
+    results = []
+    for i in range(n):
+        r = sklearn.cluster.dbscan(X=vector, eps=epsilons[i], min_samples=min_pts)
+        results.append(r[1])
 
     return results
+
+
+def get_df_for_test_results(results, class_vector, eps_start, eps_end, n):
+    df = pd.DataFrame()
+
+    epsilons = get_epsilon_array(eps_start, eps_end, n)
+    for el_index, result in enumerate(results):
+        # current_epsi = epsilon_start + el_index * (epsilon_end - epsilon_start) / n
+        for num in range(len(DATA)):
+            cluster = []
+            first_occ = class_vector.index(DATA[num])
+            last_occ = next(i for i in reversed(range(len(class_vector))) if class_vector[i] == DATA[num])
+            for point in result[first_occ:last_occ]:
+                cluster.append(point)
+            name = 'eps_' + str(round(epsilons[el_index], 2)) + '_' + DATA[num]
+            tmp_df = pd.DataFrame({name: cluster})
+            df = pd.concat([df, tmp_df], ignore_index=False, axis=1)
+
+    return df
+
+
+def get_df_for_results(results, class_vector, epsilon_start, epsilon_end, n):
+    df = pd.DataFrame()
+    epsilons = get_epsilon_array(epsilon_start, epsilon_end, n)
+
+    for el_index, result in enumerate(results):
+        # current_epsi = epsilon_start + el_index * (epsilon_end - epsilon_start) / n
+        for num in range(len(DATA)):
+            cluster = []
+            first_occ = class_vector.index(DATA[num])
+            last_occ = next(i for i in reversed(range(len(class_vector))) if class_vector[i] == DATA[num])
+            for point in result[first_occ:last_occ]:
+                cluster.append(int(point.label))
+            name = 'eps_' + str(round(epsilons[el_index], 2)) + '_' + DATA[num]
+            tmp_df = pd.DataFrame({name: cluster})
+            df = pd.concat([df, tmp_df], ignore_index=False, axis=1)
+
+    return df
+
+
+def get_purity_for_n_results(class_vector, results, number):
+    scores = []
+    for i in range(number):
+        scores.append(purity_score(class_vector, results[i]))
+
+    return scores
+
+
+def purity_score(y_true, y_pred):
+    # Source: https://stackoverflow.com/a/51672699
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+
+def get_epsilon_array(eps_start, eps_end, number):
+    c = (eps_end - eps_start) / number
+    return [eps_start + i * c for i in range(number)]
+
+
 if __name__ == '__main__':
     pass
