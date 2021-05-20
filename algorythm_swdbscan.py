428725354437410257f97c20e7045e2757d86a5d
Agnieszka Jurkiewicz <agnieszka.jurkiewicz.stud@pw.edu.pl>
2021-05-21 00:24:05 +0200

    Try to get matrix M - not working code

--- algorythm_swdbscan.py
+++ algorythm_swdbscan.py
@@ -11,7 +11,9 @@ class Cell:
         self.id = id
         self.min_coordinates = min
         self.max_coordinates = max
-        number_of_points = 0
+        self.number_of_points = 0
+        self.visited = 0
+
 
 
 def make_initial_cells():
@@ -24,9 +26,12 @@ def make_initial_cells():
 
 
 
-def compute_min_max(data):
+def compute_min_max(data, displacement, degree_of_root, n):
     max_coor = []
     min_coor = []
+    displacement_value = 0
+    if displacement == 1:
+        displacement_value = int((math.pow(n, 1/degree_of_root))/2)
     for i in range(0, len(data[0].cooridantes)):
         max_coor[i] = data[0].cooridantes[i]
         min_coor[i] = data[0].cooridantes[i]
@@ -36,16 +41,19 @@ def compute_min_max(data):
                 max_coor[k] = data[j].cooridantes[k]
             if data[j].cooridantes[k] < min_coor[k]:
                 min_coor[k] = data[j].cooridantes[k]
+    for i in range(0, len(min_coor)):
+        min_coor[i] = min_coor[i] + displacement_value
+        max_coor[i] = max_coor[i] + displacement_value
     return min_coor, max_coor
 
 
-def divide_grid(data):
+def divide_grid(data, displacement):
     n = len(data)
     degree_of_root = 2 * data[0].cooridantes
     number_of_cells = math.pow(n, 1/2)
     number_to_division = int(math.pow(n, 1/degree_of_root))
     range_division = []
-    min_coor, max_coor = compute_min_max(data)
+    min_coor, max_coor = compute_min_max(data, displacement, degree_of_root, n)
     for m in range(0, len(max_coor)):
         range_division[m] = (max_coor[m] - min_coor[m])/number_to_division
         #policzyć współrzędne komórek
@@ -71,8 +79,8 @@ def is_point_on_cell(cell,point):
     return True
 
 
-def put_point_into_cell(data):
-    cells_list = divide_grid(data)
+def put_point_into_cell(data, displacement):
+    cells_list = divide_grid(data, displacement)
     for point in data:
         for cell in cells_list:
             is_in_cell = is_point_on_cell(cell,point)
@@ -95,59 +103,64 @@ def find_max_number_of_points(cells_list):
     return list_of_cells_with_max_number_of_cells, max_points
 
 
-def grid_clustering(data, minPts, eps):
-    cells_list = put_point_into_cell(data)
+def grid_clustering(data, minPts, eps, displacement):
+    cells_list = put_point_into_cell(data, displacement)
     list_of_cells_with_max_number_of_cells, max_points = find_max_number_of_points(cells_list)
     divider = 5
     threshold = max_points/divider
-    result = []
+    #result = []
     for cell in list_of_cells_with_max_number_of_cells:
         algorythm_tidbscan(minPts, eps, cell.points)
+        cell.visited = 1
     for i in range(1, divider + 1):
         eps = eps + 0.1 * eps
+        min_range = max_points - i * threshold
+        max_range = max_points - (i - 1) * threshold
+        for cell in cells_list:
+            if cell.visited == 0 and (cell.number_of_points >= min_range and cell.number_of_points < max_range):
+                algorythm_tidbscan(minPts, eps, cell.points)
+    return data
 
+
+def get_matrix_M(max_cluster_label_1, max_cluster_label_2, dataBase_1, dataBase_2):
+    list_1 = []
+    for i in range(0, len(max_cluster_label_1 + 1)):
+        list_2 = []
+        for j in range(0, len(max_cluster_label_2 + 1)):
+            matrix_M[max_cluster_label_1 + 1, max_cluster_label_2 + 1] = list
+    for i in range(0, len(dataBase_1)):
+        for j in range(0, len(dataBase_2)):
+            if dataBase_1[i].id == dataBase_2[j].id:
+                matrix_M[dataBase_1[i].cell_number, dataBase_2[j].cell_number].append(dataBase_1[i])
+                break
+    return matrix_M
 
 
 def algorythm_swdbscan(minPts, eps, data):
-    clusterId = 0
-    dataBase = read_database(data)
-    data_base_sort_with_ref_point = distance_from_ref_point(dataBase)
-    # print(f'data_base_sort_with_ref_point[0]: , {data_base_sort_with_ref_point[0].id}')
-    # print(f'data_base[0]: , {dataBase[0].id}')
+    max_cluster_label_1 = -1
+    max_cluster_label_2 = -1
+    dataBase_1 = read_database(data)
+    dataBase_2 = dataBase_1.copy()
+    grid_clustering(dataBase_1, minPts, eps, 0)
+    grid_clustering(dataBase_2, minPts, eps, 1)
+    for i in range(0, len(dataBase_1)):
+        if dataBase_1[i].label > max_cluster_label_1:
+            max_cluster_label_1 = dataBase_1[i].label
+        if dataBase_2[i].label > max_cluster_label_2:
+            max_cluster_label_2 = dataBase_2[i].label
+    matrix_M = get_matrix_M(max_cluster_label_1, max_cluster_label_2, dataBase_1, dataBase_2)
+    clusters_matrix = np.empty((2, max_cluster_label_2 + 1, 0), dtype=np.int32)
+    for k in range(0, max_cluster_label_2 + 1):
+        for m in range(0, max_cluster_label_1):
+            if matrix_M[m, k] < minPts:
+                noise = noise + matrix_M[m, k]
+            else:
+                group = group + matrix_M[m, k]
+        clusters_matrix[0, k] = group
+        clusters_matrix[1, k] = noise
 
-    for point in dataBase:
-        if point.label != "UNDEFINED":
-            continue
-        neighbors = rangeQuery(point, eps, data_base_sort_with_ref_point)
-        if len(neighbors) < minPts - 1:
-            point.label = -1
-            continue
-        if len(neighbors) >= minPts - 1:
-            point.label = clusterId
-            seedSet = neighbors
-        while seedSet:
-            seedPoint = seedSet.pop()
-            if dataBase[seedPoint.id].label == -1:
-                dataBase[seedPoint.id].label = clusterId
-                continue
-            if dataBase[seedPoint.id].label == "UNDEFINED":
-                neighborsForSeedPoint = rangeQuery(seedPoint, eps, data_base_sort_with_ref_point)
-                if len(neighborsForSeedPoint) < minPts - 1:
-                    dataBase[seedPoint.id].label = clusterId
-                    continue
-                if len(neighborsForSeedPoint) >= minPts - 1:
-                    dataBase[seedPoint.id].label = clusterId
-                    seed = seedSet + neighborsForSeedPoint
-                    seedSet = seed
-                    # print("set")
-                    # for i in seedSet:
-                        # print(f'Seed: {i.id}')
-                    # print("end set")
-                    continue
-                continue
-        clusterId += 1
 
-    return dataBase
+    return data
 
 
 def print_hi(name):
